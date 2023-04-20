2ed957d9b370eec88255bb054487a0080d9f2884
Arthas Menethil <90930371+AinsOoalGown@users.noreply.github.com>
2023-04-20 21:45:24 +0200

    Add files via upload

--- Area_espera.cc
+++ Area_espera.cc
@@ -8,15 +8,16 @@ Area_espera::Area_espera() {
 }
 
 void Area_espera::add_job(Proceso p, string id_prior) {
-
+    mprior[id_prior].add_job(p);
 }
 
 void Area_espera::add_prior(string id_prior) {
-
+    Prioridad p (id_prior);
+    mprior.insert(make_pair(id_prior, p));
 }
 
 void Area_espera::eliminar_prior(string id_prior) {
-
+    mprior.erase(id_prior);
 }
 
 void Area_espera::enviar_job_a_cluster() {
@@ -28,15 +29,15 @@ bool Area_espera::pendiente_global() const {
 }
 
 bool Area_espera::id_prior_pendiente (string id_prior) const {
-    return false;
+    return mprior.at(id_prior).en_espera();
 }
 
 bool Area_espera::existe_prior(string id_prior) const {
-    return false;
+    return (mprior.find(id_prior) != mprior.end());
 }
 
 bool Area_espera::existe_prior_job(string id_prior, int id) const {
-    return false;
+   return mprior.at(id_prior).existe_job(id);
 }
 
 void Area_espera::leer() {
@@ -52,22 +53,17 @@ void Area_espera::leer() {
 
 void Area_espera::escribir() const {
     for (map <string, Prioridad>::const_iterator it = mprior.begin(); it != mprior.end(); ++it) {
+        cout << (*it).first << endl;    
         escribir_prior((*it).first);
     }
 
 }
 
 void Area_espera::escribir_prior(string id_prior) const {
-    cout << id_prior;
     Prioridad p = mprior.at(id_prior);
     if (p.en_espera()) p.escribir_job();   
-    cout << endl; 
     p.escribir_enviados();
     cout << ' ';
     p.escribir_rechazados();
     cout << endl;
-}
-
-void Area_espera::escribir_intentos(string id_prior) const {
-
 }
